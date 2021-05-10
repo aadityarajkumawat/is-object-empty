e45b03e44fa6b97c79f9174c202abf022c622659
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-05-10 17:03:12 +0530

    added arg protection

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 "use-strict";
 
 function isObjectEmpty(object) {
+    if (object instanceof RegExp || object !== Object(object)) {
+        throw new Error(
+            `isObjectEmpty accepts an object as an argument while ${object} was found`
+        );
+    }
+
     const keys = Object.keys(object);
 
     if (keys.length > 0) {
