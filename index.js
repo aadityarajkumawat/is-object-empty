13923ae9949bfa3b85eeca1fd0b304c480048af1
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-05-10 14:12:56 +0530

    done with logic of function

---
+++ index.js
@@ -0,0 +1,12 @@
+"use-strict";
+
+const isObjectEmpty = (object) => {
+    const keys = Object.keys(object);
+
+    if (keys.length > 0) {
+        return false;
+    }
+    return true;
+};
+
+module.exports.isObjectEmpty = isObjectEmpty;
